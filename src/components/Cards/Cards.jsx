3d72859e5a7cc86f8c9cee59c08a79c28632c5b5
Michael Singh <singh.michaelanthony@gmail.com>
2020-07-18 12:31:24 -0400

    display time in milliseconds as Locale String

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -9,17 +9,17 @@ const Cards = ( {orders: { orders }} ) => {
     if(!orders) {
         return 'Loading...';
     }
-    
+        
     return (
         <div className={styles.container}>
             <Grid container spacing={4} justify="center">
             {orders.map(({ deadline, description, name, workerId }, i) => (
-                    <Grid key={i} item component={Card} xs={12} md={2} className={styles.card} >
+                    <Grid key={i} item component={Card} xs={12} md={3} className={styles.card} >
                         <CardContent>
                             <Typography variant="h5" gutterBottom>{name}</Typography>
                             <Typography variant="body2" color="textSecondary">{description}</Typography>
                             <Typography variant="h6">{workerId}</Typography>
-                            <Typography color="textSecondary">{new Date(deadline).toDateString()}</Typography>
+                            <Typography color="textSecondary">{new Date(deadline).toLocaleString()}</Typography>
                         </CardContent>
                     </Grid>
                 ))}
